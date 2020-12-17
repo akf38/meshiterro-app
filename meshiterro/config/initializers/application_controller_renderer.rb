bad81019004b0f5761f096458d55e0004ada2c6a
akf38 <dpv28779@gmail.com>
2020-12-17 14:58:55 +0000

    [Start]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9b3a4bec3a09085fc59d364ef5df57a8464bd476f72158211efd3ea8306905bcfe17a4e03d9d64ede14c28a9db613a15b654a9746cf88a08520ea7b66808fcb2'
